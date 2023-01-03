7734b724e1e450e1b98a6f9ad7ef9d9ed46a2bd6
jsteinberg7 <j.steinberg702@gmail.com>
2023-01-02 21:01:02 -0500

    first day of changes

--- app.py
+++ app.py
@@ -1,8 +1,45 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def home():
-    return 'home'
+    # return the home.html template
+    return render_template('home.html')
+
+@app.route('/', methods=['POST'])
+def get_mf_form():
+    height = request.form['height']
+    weight = request.form['weight']
+    bmi = calculateBMI(height, weight)
+    return "Your BMI is: " + str(bmi)
+
+# def my_form_post(text):
+#     text = request.form['text']
+#     return text
+
+# Calculate BMI
+# Height is in meters
+# Weight is in kilograms
+def calculateBMI(height: int, weight: int) -> float:
+    height = int(height)
+    weight = int(weight) 
+    if (height < 200 and weight < 500):
+        # Convert height and weight to metric system
+        height *= 0.0254
+        weight *= 0.453592
+        bmi: float = weight / (height * height)
+    else: 
+         bmi = "Incorrect Input."
+    return bmi
+
+# if __name__ == '__main__':
+#    print(calculateBMI(76, 200))
+    
+    
+
+    
+
+
 
     
